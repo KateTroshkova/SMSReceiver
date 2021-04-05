63cac01518b65028016aad3bd26a56d33edb8233
etroshkova <Ekaterina.Troshkova@noveogroup.com>
2021-04-05 18:25:49 +0700

    Implement content resolver (works)

--- app/src/main/java/com/android/research/MainActivity.kt
+++ app/src/main/java/com/android/research/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.android.research
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Telephony.TextBasedSmsColumns.ADDRESS
+import android.provider.Telephony.TextBasedSmsColumns.BODY
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermissions()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_CODE && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+            readSMS()
+        }
+    }
+
+    private fun requestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_MMS) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_WAP_PUSH) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.READ_SMS,
+                    Manifest.permission.RECEIVE_SMS,
+                    Manifest.permission.SEND_SMS,
+                    Manifest.permission.RECEIVE_MMS,
+                    Manifest.permission.RECEIVE_WAP_PUSH),
+                PERMISSION_CODE)
+        } else {
+            readSMS()
+        }
+    }
+
+    private fun readSMS() {
+        val sms = mutableListOf<String>()
+        val contentResolver = contentResolver ?: return
+        val cursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null)
+            ?: return
+        val indexBody = cursor.getColumnIndex(BODY)
+        val indexAddr = cursor.getColumnIndex(ADDRESS)
+        if (indexBody < 0 || !cursor.moveToFirst()) return
+        do {
+            val str = "Sender: " + cursor.getString(indexAddr) + "\n" + cursor.getString(indexBody)
+            sms.add(str)
+        } while (cursor.moveToNext())
+        cursor.close()
+    }
+
+    private companion object {
+        private const val PERMISSION_CODE = 24
     }
 }
